988fd16ebd522a2ce08626a46062fdbde5e5827c
massyaf03 <omarbnzaky@gmail.com>
2019-12-02 17:06:33 +0200

    group iles

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
-import misaka 
+import misaka
 from django.contrib.auth import get_user_model
 from django import template
 from groups.models import Group
@@ -13,7 +13,7 @@ class Post(models.Model):
 	 message = models.TextField()
 	 message_html = models.TextField(editable=False)
 	 group = models.ForeignKey(Group,related_name='posts',null=True,blank=True)
-		
+
 	 def __str__(self):
 	 	return self.message
 
@@ -24,8 +24,8 @@ class Post(models.Model):
  	def get_absolute_url(self):
 	return reverse('posts:single',kwargs={'username':self.user.username,
 											'pk':self.pk})
+	#pk is for a primary key
 
 	class Meta:
-		ordering = ['created_at']
+		ordering = ['-created_at']
 		unique_together= ['group','user']
-			
