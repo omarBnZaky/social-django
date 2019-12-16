2308bd670959e85b7223c1fbe99d46700540a69a
omar <omarbnzaky@gmail.com>
2019-12-16 11:54:51 +0200

    trying to upload image

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings # new
+from django.conf.urls.static import static # new
+
 app_name = 'posts'
+
 
 urlpatterns = [
 	path('',views.PostList.as_view(),name='all'),
@@ -9,3 +13,6 @@ urlpatterns = [
 	path('by/(?P<username>[-\w]+)/(?P<pk>\d+)/',views.PostDetail.as_view(),name='single'),
 	path('delete/(?P<pk>\d+)/',views.DeletePost.as_view(),name='delete')
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social/urls.py
+++ social/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.HomePage.as_view(),name='home'),
@@ -26,3 +27,4 @@ urlpatterns = [
     path('test/',views.TestPage.as_view(),name='test'),
     path('thanks/',views.ThanksPage.as_view(),name='thanks'),
 ]
+
